83add1fa9eecf2e8737a1872e4095a3fe9bbf526
horsicq <horsicq@gmail.com>
2022-05-30 18:01:55 +0200

    Fix: 2022-05-30

--- gui_source/main_gui.cpp
+++ gui_source/main_gui.cpp
@@ -22,7 +22,7 @@
 #include <QApplication>
 #include <QStyleFactory>
 
-int main(int argc, char *argv[])
+int main(int argc,char *argv[])
 {
 #if QT_VERSION >= QT_VERSION_CHECK(5,6,0)
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
